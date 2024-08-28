98d1375a974f19c98ffce056fc9385daa18f2a1b
lilydeyoung <ldeyoung01@saintmarys.edu>
2024-08-28 17:11:09 -0400

    Add files via upload

---
+++ Lab01.py
@@ -0,0 +1,79 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+"Hello World!"
+'Hello World!'
+"Hello Lily!"
+'Hello Lily!'
+2+2
+4
+3.14+0.1
+3.24
+>>> 3.14-0.14
+3.0
+>>> print("Hello")
+Hello
+>>> 
+============ RESTART: C:/Users/lilym/Desktop/CPSC 207/Lab1.py ===========
+Welcome to CPSC 207!
+>>> #"Lily DeYoung and Aliza Litvak"
+>>> print("Hello, world")
+Hello, world
+>>> print("Welcome to CPSC 207")
+Welcome to CPSC 207
+>>> print("Hello\nWorld")
+Hello
+World
+>>> print("How\nare\nyou")
+How
+are
+you
+>>> print("Today","is","Wednesday")
+Today is Wednesday
+>>> print("Today" + "is" + "Wednesday")
+TodayisWednesday
+>>> print("a\nb\nc")
+a
+b
+c
+>>> print("Hello"+"World")
+HelloWorld
+>>> print("Hello\n"+"\nWorld!")
+Hello
+
+World!
+>>> print("123\n45\n6")
+123
+45
+6
+>>> 1+2*3
+7
+>>> (1+2)*3
+9
+>>> 7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22/4
+5.5
+22//4
+5
+22%4
+2
+3*2
+6
+3**2
+9
+2**3
+8
+(4+3)//2
+3
+6%2
+0
+1+2*3**4
+163
+print("2+2=",2+2)
+2+2= 4
+print("50 pounds is ",50*0.453592,"kilograms")
+50 pounds is  22.6796 kilograms
